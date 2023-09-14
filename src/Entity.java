c4093847c7cd01612d75513d3b870605fc72dde9
Geoffray Gaborit <gaborit.geoffray@gmail.com>
2023-09-14 16:12:22 +0200

    feat: add abstract entity class

---
+++ src/Entity.java
@@ -0,0 +1,27 @@
+public abstract class Entity {
+  private String name;
+  private int score;
+
+  public Entity() {
+    this.name = "";
+    this.score = 0;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getScore() {
+    return this.score;
+  }
+
+  public void incrementScore() {
+    this.score++;
+  }
+
+  abstract public int selectSign();
+}
