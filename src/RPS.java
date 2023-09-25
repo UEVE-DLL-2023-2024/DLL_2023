109bc466a6b4d760226bc78df34407010ee6c73f
SIBY <siby@MacBook-Air-de-SIBY.local>
2023-09-25 16:20:36 +0200

    feature: add Multiplayer

--- src/RPS.java
+++ src/RPS.java
@@ -1,72 +1,117 @@
+import java.util.Scanner;
+
 public class RPS {
-	private Player player;
+	private Player player1;
+	private Player player2;
 	private Bot bot;
 	private Scoreboard scoreboard;
+
+	private int mode;
 	
 	public RPS() {
-		player = new Player();
+		player1 = new Player();
+		player2 = new Player();
 		bot = new Bot();
-		scoreboard = new Scoreboard(player, bot);
+		scoreboard = new Scoreboard(player1, bot);
 	}
 
 	public static void main(String[] args) {
 		RPS rps = new RPS();
-		rps.getPlayerName();
 		rps.getNumberOfRound();
+		rps.chooseGameMode();
 		rps.startGame();
 	}
 
-	// Fonction appelee recursivement tant que le joueur souhaite jouer
-	public void startGame() {
-		int playerSign = player.selectSign();
-		Controller.display(player.getName(), playerSign);
-		int botSign = bot.selectSign();
-		Controller.display("bot", botSign);
+	// Choix du mode de jeu
+	public void chooseGameMode() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Choisissez le mode de jeu :");
+		System.out.println("1. Contre un autre joueur");
+		System.out.println("2. Contre le bot");
+		mode = sc.nextInt();
 
-		int result = Controller.compareSigns(playerSign, botSign);
-		switch (result) {
-		case 0: 
-			System.out.println("Egalite !");
-			break;
-		case 1: 
-			System.out.println(player.getName()+ " remporte la manche !");
-			player.incrementScore();
-			break;
-		case -1: 
-			bot.incrementScore();
-			System.out.println("Bot remporte la manche !");
-			break;
+		if (mode == 1) {
+			System.out.println("Joueur 1, entrez votre nom :");
+			player1.askName();
+			System.out.println("Joueur 2, entrez votre nom :");
+			player2.askName();
+		} else if (mode == 2) {
+			System.out.println("Joueur, entrez votre nom :");
+			player1.askName();
+		} else {
+			System.out.println("Mode de jeu invalide. Choisissez 1 ou 2.");
+			chooseGameMode();
 		}
+	}
 
-		scoreboard.displayScore();
+	// Fonction appelee recursivement tant que les joueurs souhaitent jouer
+	public void startGame() {
+		int player1Sign = player1.selectSign();
+		Controller.display(player1.getName(), player1Sign);
+		if (mode == 2) {
+			scoreboard.displayScore();
 
-		// Demande a l'utilisateur s'il veut jouer a nouveau
-		int nbRound = player.getNbOfRound();
-		int playerCount = player.getScore();
-		int botCount = bot.getScore();
-		if (playerCount == nbRound || botCount == nbRound){
-			System.out.println("Fin du jeu");
-			player.resetScore();
-			bot.resetScore();
-			if (player.playAgain()) {
-				System.out.println();
-				getNumberOfRound();
-				startGame();
-			} else {
-				System.out.println("Fin du jeu !");
+			// Demande a l'utilisateur s'il veut jouer a nouveau
+			int nbRound = player1.getNbOfRound();
+			int playerCount = player1.getScore();
+			int botCount = bot.getScore();
+			if (playerCount == nbRound || botCount == nbRound){
+				System.out.println("Fin du jeu");
+				player1.resetScore();
+				bot.resetScore();
+				if (player1.playAgain()) {
+					System.out.println();
+					getNumberOfRound();
+					startGame();
+				} else {
+					System.out.println("Fin du jeu !");
+				}
+			 }
+			 else {
+				 startGame();
 			}
-		 }
-		 else { 
-			startGame();
+		} else if (mode == 1) {
+			if (player2.getName() != null) {
+				int player2Sign = player2.selectSign();
+				Controller.display(player2.getName(), player2Sign);
+				int result = Controller.compareSigns(player1Sign, player2Sign);
+				displayResult(result, player1.getName(), player2.getName());
+			} else {
+				int botSign = bot.selectSign();
+				Controller.display("Bot", botSign);
+				int result = Controller.compareSigns(player1Sign, botSign);
+				displayResult(result, player1.getName(), "Bot");
+			}
+
+			// Jouer avec qui ?
+			if (player1.playAgain()) {
+				System.out.println();
+				startGame();
+			}
 		}
 	}
 
 	// Fonction utilisee pour demander le nom du joueur
 	public void getPlayerName() {
-		player.askName();
+		 player1.askName();
+	}
+
+	// Fonction pour afficher le résultat
+	public void displayResult(int result, String player1Name, String player2Name) {
+		switch (result) {
+			case 0:
+				System.out.println("Egalite !");
+				break;
+			case 1:
+				System.out.println(player1Name + " remporte la manche !");
+				break;
+			case -1:
+				System.out.println(player2Name + " remporte la manche !");
+				break;
+		}
 	}
 	
 	public void getNumberOfRound(){
-		player.askNumberOfRound();
+		player1.askNumberOfRound();
 	}
 }
