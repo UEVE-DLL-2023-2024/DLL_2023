e82c3938444ee0bc6844ac1cb89513fd7b106422
Grunt471 <matheo.delenin@gmail.com>
2023-09-26 12:47:04 +0200

    feat : ajout des nouveaux signes dans Controller

--- src/Controller.java
+++ src/Controller.java
@@ -2,6 +2,8 @@ public class Controller {
 	public static final int PIERRE = 1;
 	public static final int FEUILLE = 2;
 	public static final int CISEAUX = 3;
+	public static final int LEZARD = 4;
+	public static final int SPOCK = 5;
 
 	// Fonction affichant le signe choisi
 	public static void display(String someone, int sign) {
@@ -15,6 +17,12 @@ public class Controller {
 		case Controller.CISEAUX:
 			System.out.print(someone + " a choisi CISEAUX, ");
 			break;
+		case Controller.LEZARD:
+			System.out.print(someone + " a choisi LEZARD, ");
+			break;
+		case Controller.SPOCK:
+			System.out.print(someone + " a choisi SPOCK, ");
+			break;
 		default:
 			break;
 		}
@@ -25,11 +33,15 @@ public class Controller {
 		if (playerSign == botSign) return 0;
 		switch (playerSign) {
 		case PIERRE:
-			return (botSign == CISEAUX ? 1 : -1);
+			return (botSign == CISEAUX || botSign == LEZARD ? 1 : -1);
 		case FEUILLE:
-			return (botSign == PIERRE ? 1 : -1);
+			return (botSign == PIERRE || botSign == SPOCK ? 1 : -1);
 		case CISEAUX:
-			return (botSign == FEUILLE ? 1 : -1);
+			return (botSign == FEUILLE || botSign == LEZARD ? 1 : -1);
+		case LEZARD:
+			return (botSign == SPOCK || botSign == FEUILLE ? 1 : -1);
+		case SPOCK:
+			return (botSign == CISEAUX || botSign == PIERRE ? 1 : -1);
 		}
 		return 0;
 	}
